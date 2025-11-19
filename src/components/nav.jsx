7a05e5da5cd6d82a9f65623491882c819ccc5b8b
Dung3t <tiendung3t#gmail.com>
2025-11-19 14:33:34 +0700

    update navigation

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,12 +1,33 @@
+import { useState } from "react";
+
 export default function Nav() {
+  const [disabledHover, setDisabledHover] =  useState(false);
+
+  const handleClick = () => {
+    setDisabledHover(prev => !prev);
+  }
   return (
     <nav className="navigation">
       <div className="nav-list">
-        <img src="/src/assets/add-post.png" className="new-chat" alt="new chat" />
+        <div 
+          className={`nav-item-1 ${disabledHover ? "nav-item-disabled" : ""}`} 
+          onClick={handleClick}
+        >
+          <img 
+            src={disabledHover ? "/src/assets/add-post.png" : "/src/assets/app.png"} 
+            alt="new chat" 
+          />
+        </div>
         <hr />
-        <img src="/src/assets/messenger.png" className="chat-ai" alt="chat ai" />
+        <div className="nav-item-2">
+          <img src="/src/assets/messenger.png" className="chat-ai" alt="chat ai" />
+          <div>Chat AI Health</div>
+        </div>
         <hr />
-        <img src="/src/assets/hospital.png" className="check-health" alt="check health" />
+        <div className="nav-item-3">
+          <img src="/src/assets/hospital.png" className="check-health" alt="check health" />
+          <div>Check health</div>
+        </div>    
         <hr />
       </div>
       <div className="nav-text">
