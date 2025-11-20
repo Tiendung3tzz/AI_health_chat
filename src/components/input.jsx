0f8231abdf0015a88f88ddf7d0a0c6f616ac297d
Dung3t <tiendung3t#gmail.com>
2025-11-20 13:40:48 +0700

    create input UI

---
+++ src/components/input.jsx
@@ -0,0 +1,9 @@
+export default function Search() {
+    return (
+        <div className="input-container">
+            <img src="/src/assets/add.png" className="add" alt="add" />
+            <input type="text" placeholder="Are you question..." />
+            <img src="/src/assets/login.png" className="enter" alt="enter" />
+        </div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from './components/Header.jsx'
 import Nav from './components/nav.jsx'
+import Search from './components/input.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
     <Nav />
+    <Search />
   </StrictMode>,
 )
